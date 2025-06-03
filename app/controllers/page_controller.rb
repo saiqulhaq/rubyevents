8699378ec47fab13fb2262bd1fbc9d9e8443d09c
Marco Roth <marco.roth@intergga.ch>
2025-06-04 00:18:00 +0200

    Use `Events#with_talks` scope in `PageController`

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -35,7 +35,7 @@ class PageController < ApplicationController
     @featured_events = Event.distinct
       .includes(:organisation)
       .where(slug: playlist_slugs)
-      .where.associated(:talks)
+      .with_talks
       .in_order_of(:slug, playlist_slugs)
 
     respond_to do |format|
