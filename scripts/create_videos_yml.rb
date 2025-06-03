13e64e4e03e389b5ca4cdd4b72cb956bd788ceb8
Marco Roth <marco.roth@intergga.ch>
2025-06-03 23:05:40 +0200

    Add `create_videos_yml` script

---
+++ scripts/create_videos_yml.rb
@@ -0,0 +1,17 @@
+# start this script with the rails runner command
+# $ rails runner scripts/create_videos_yml.rb [playlist_id]
+#
+
+playlist_id = ARGV[0]
+
+if playlist_id.blank?
+  puts "Please provide a playlist id"
+  exit 1
+end
+
+puts Youtube::PlaylistItems.new
+  .all(playlist_id: playlist_id)
+  .map { |metadata| Youtube::VideoMetadata.new(metadata: metadata, event_name: "TODO").cleaned }
+  .map { |item| item.to_h.stringify_keys }
+  .to_yaml
+  .gsub("- title:", "\n- title:") # Visually separate the talks with a newline
