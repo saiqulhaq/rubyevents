959edfd9b61645801860df617a33795ce32c8503
Marco Roth <marco.roth@intergga.ch>
2025-06-04 18:30:23 +0200

    More defensive checks for country controller

--- app/controllers/events/countries_controller.rb
+++ app/controllers/events/countries_controller.rb
@@ -2,12 +2,12 @@ class Events::CountriesController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
 
   def index
-    @countries_by_continent = Event.all.map { |event| event.static_metadata.country }.uniq.group_by { |country| country&.continent || nil }.sort_by { |key, _value| key || "ZZ" }.to_h
-    @events_by_country = Event.all.sort_by { |event| event.static_metadata.home_sort_date }.reverse.group_by { |event| event.static_metadata.country }.sort_by { |key, value| key&.iso_short_name || "ZZ" }.to_h
+    @countries_by_continent = Event.all.map { |event| event.static_metadata&.country }.uniq.group_by { |country| country&.continent || "Unknown" }.sort_by { |key, _value| key || "ZZ" }.to_h
+    @events_by_country = Event.all.sort_by { |event| event.static_metadata&.home_sort_date }.reverse.group_by { |event| event.static_metadata&.country || "Unknown" }.sort_by { |key, value| (key.is_a?(String) ? key : key&.iso_short_name) || "ZZ" }.to_h
   end
 
   def show
     @country = Country.find(params[:country])
-    @events = Event.all.select { |event| event.static_metadata.country == @country }.sort_by { |event| event.static_metadata.home_sort_date }.reverse
+    @events = Event.all.select { |event| @country.present? && event.static_metadata&.country == @country }.sort_by { |event| event.static_metadata&.home_sort_date || Time.at(0).to_date }.reverse
   end
 end
