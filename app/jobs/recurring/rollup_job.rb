ce7dbeb59818847bcfda803c53a3bcaefafe252d
Adrien Poly <adrienpoly@gmail.com>
2025-08-08 02:01:16 +0200

    clean up suspicious visits before computing rollups

--- app/jobs/recurring/rollup_job.rb
+++ app/jobs/recurring/rollup_job.rb
@@ -2,10 +2,30 @@ class Recurring::RollupJob < ApplicationJob
   queue_as :low
 
   def perform(*args)
+    # first we remove suspicious visits
+    cleanup_suspicious_recent_visits
+
+    # then we rollup the visits and events
     Ahoy::Visit.rollup("ahoy_visits", interval: :day)
     Ahoy::Visit.rollup("ahoy_visits", interval: :month)
     Ahoy::Event.rollup("ahoy_events", interval: :day)
     Ahoy::Event.rollup("ahoy_events", interval: :month)
     Talk.rollup("talks", interval: :year, column: :date)
+  end
+
+  def cleanup_suspicious_recent_visits
+    # Find IPs with more than 50 visits in the last 3 days
+    suspicious_ips = Ahoy::Visit
+      .where(started_at: 3.day.ago..)
+      .group(:ip)
+      .having("COUNT(*) > 50")
+      .pluck(:ip)
+
+    if suspicious_ips.any?
+      visits = Ahoy::Visit.where(ip: suspicious_ips)
+
+      Ahoy::Event.where(visit_id: visits.select(:id)).delete_all
+      visits.delete_all
+    end
   end
 end
