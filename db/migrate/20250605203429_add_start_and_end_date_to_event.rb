6b0caca2c482e0176a71d91ddc4835d262f21101
Adrien Poly <adrienpoly@gmail.com>
2025-08-10 22:34:59 +0200

    fix migration (#891)

--- db/migrate/20250605203429_add_start_and_end_date_to_event.rb
+++ db/migrate/20250605203429_add_start_and_end_date_to_event.rb
@@ -2,9 +2,5 @@ class AddStartAndEndDateToEvent < ActiveRecord::Migration[8.0]
   def change
     add_column :events, :start_date, :date
     add_column :events, :end_date, :date
-
-    Event.all.each do |event|
-      event.update(start_date: event.static_metadata.start_date, end_date: event.static_metadata.end_date)
-    end
   end
 end
--- db/migrate/20250617115359_add_date_precision_to_events.rb
+++ db/migrate/20250617115359_add_date_precision_to_events.rb
@@ -3,5 +3,21 @@
 class AddDatePrecisionToEvents < ActiveRecord::Migration[8.0]
   def change
     add_column :events, :date_precision, :string, null: false, default: "day"
+
+    Event.all.each do |event|
+      event.update(start_date: event.static_metadata.start_date, end_date: event.static_metadata.end_date)
+    end
+
+    Event.find_in_batches.each do |events|
+      events.each do |event|
+        if event.static_metadata.meetup?
+          event.update!(kind: "meetup")
+        elsif event.static_metadata.conference?
+          event.update!(kind: "conference")
+        else
+          event.update!(kind: "event")
+        end
+      end
+    end
   end
 end
