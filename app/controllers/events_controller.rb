f5a176397a8e4684adffc89a843d005066442b0d
Marco Roth <marco.roth@intergga.ch>
2025-08-10 10:43:51 +0200

    Sample meetup talks and meetup speakers

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -21,8 +21,8 @@ class EventsController < ApplicationController
       all_meetup_events = @event.talks.where(meta_talk: true).includes(:speakers, :parent_talk, child_talks: :speakers)
       @upcoming_meetup_events = all_meetup_events.where("date >= ?", Date.today).order(date: :asc).limit(4)
       @recent_meetup_events = all_meetup_events.where("date < ?", Date.today).order(date: :desc).limit(4)
-      @recent_talks = @event.talks.where(meta_talk: false).includes(:speakers, :parent_talk, child_talks: :speakers).order(date: :desc).to_a.shuffle.first(8)
-      @featured_speakers = @event.speakers.joins(:talks).distinct.to_a.shuffle.first(8)
+      @recent_talks = @event.talks.where(meta_talk: false).includes(:speakers, :parent_talk, child_talks: :speakers).order(date: :desc).to_a.sample(8)
+      @featured_speakers = @event.speakers.joins(:talks).distinct.to_a.sample(8)
     else
       @keynotes = @event.talks.joins(:speakers).where(kind: "keynote").includes(:speakers, event: :organisation)
       @recent_talks = @event.talks.watchable.includes(:speakers, event: :organisation).limit(8).shuffle
