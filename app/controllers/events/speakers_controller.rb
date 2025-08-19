33ecc4b53db0f1aa84e9ef3067b9cc98cd22a89f
Marco Roth <marco.roth@intergga.ch>
2025-08-19 13:41:13 +0200

    Add Talk Count to Event Speakers Tab

--- app/controllers/events/speakers_controller.rb
+++ app/controllers/events/speakers_controller.rb
@@ -3,6 +3,22 @@ class Events::SpeakersController < ApplicationController
   before_action :set_event, only: %i[index]
 
   def index
+    if @event.meetup?
+      # For meetups, get speakers with their talk counts at this specific meetup
+      # and sort by number of talks (descending)
+      speaker_ids = @event.talks.joins(:user_talks).pluck("user_talks.user_id")
+      speaker_counts = speaker_ids.tally
+
+      @speakers_with_counts = User
+        .where(id: speaker_counts.keys)
+        .where("talks_count > 0")
+        .map do |speaker|
+          # Add the meetup-specific talk count
+          speaker.define_singleton_method(:meetup_talks_count) { speaker_counts[speaker.id] }
+          speaker
+        end
+        .sort_by { |s| [-s.meetup_talks_count, s.name] }
+    end
   end
 
   private
