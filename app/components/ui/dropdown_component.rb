ed00abc623fdaf906a719362abcb591ee7db816c
Justin Marsh <jmarsh24@gmail.com>
2025-08-21 22:30:33 +0200

    increase z-index of the dropdown (#926)

--- app/components/ui/dropdown_component.rb
+++ app/components/ui/dropdown_component.rb
@@ -57,6 +57,6 @@ class Ui::DropdownComponent < ApplicationComponent
   end
 
   def content_classes
-    class_names("dropdown-content menu menu-smp-2 mt-4 w-max z-[1] rounded-lg shadow-2xl bg-white text-neutral", attributes.delete(:content_classes))
+    class_names("dropdown-content menu menu-smp-2 mt-4 w-max z-30 rounded-lg shadow-2xl bg-white text-neutral", attributes.delete(:content_classes))
   end
 end
