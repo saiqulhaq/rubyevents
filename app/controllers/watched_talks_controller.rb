d75706db1bc853c3cdd52ec9972b7eccebde2b4f
Adrien Poly <adrienpoly@gmail.com>
2025-08-31 18:39:27 +0200

    remove n+1 on watched talk page (#959)

--- app/controllers/watched_talks_controller.rb
+++ app/controllers/watched_talks_controller.rb
@@ -2,9 +2,9 @@ class WatchedTalksController < ApplicationController
   include WatchedTalks
 
   def index
-    @talks = Current.user.watched_talks
+    @watched_talks = Current.user.watched_talks
       .includes(talk: [:speakers, {event: :organisation}, {child_talks: :speakers}])
       .order(created_at: :desc)
-      .map(&:talk)
+    @talks = @watched_talks.map(&:talk)
   end
 end
