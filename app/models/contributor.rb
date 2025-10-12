2c2bdbc75674047020a8af6776d7599475ed99ba
Adrien Poly <adrienpoly@gmail.com>
2025-10-12 17:47:05 +0200

    annotate contributor

--- app/models/contributor.rb
+++ app/models/contributor.rb
@@ -1,3 +1,25 @@
+# == Schema Information
+#
+# Table name: contributors
+#
+#  id         :integer          not null, primary key
+#  avatar_url :string
+#  html_url   :string
+#  login      :string           not null, uniquely indexed
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer          indexed
+#
+# Indexes
+#
+#  index_contributors_on_login    (login) UNIQUE
+#  index_contributors_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  user_id  (user_id => users.id)
+#
 class Contributor < ApplicationRecord
   belongs_to :user, optional: true
 
