d0da246d1b1f4339bffbb0d86e97f7d606b032e4
Adrien Poly <adrienpoly@gmail.com>
2025-10-21 07:08:42 +0200

    loosen migration

--- db/migrate/20251019071129_add_case_insensitive_github_handle_index_to_users.rb
+++ db/migrate/20251019071129_add_case_insensitive_github_handle_index_to_users.rb
@@ -1,14 +1,14 @@
 class AddCaseInsensitiveGitHubHandleIndexToUsers < ActiveRecord::Migration[8.1]
   def up
     User.transaction do
-      User.find_by!(github_handle: "bodacious").assign_canonical_user!(canonical_user: User.find_by!(github_handle: "Bodacious"))
-      User.find_by!(github_handle: "joschkaschulz").assign_canonical_user!(canonical_user: User.find_by!(github_handle: "JoschkaSchulz"))
-      User.find_by!(github_handle: "kyfast").assign_canonical_user!(canonical_user: User.find_by!(github_handle: "KyFaSt"))
-      User.find_by!(github_handle: "nabeelahy").assign_canonical_user!(canonical_user: User.find_by!(github_handle: "NabeelahY"))
-      User.find_by!(github_handle: "ryanbrushett").assign_canonical_user!(canonical_user: User.find_by!(github_handle: "RyanBrushett"))
-      User.find_by!(github_handle: "thomascountz").assign_canonical_user!(canonical_user: User.find_by!(github_handle: "Thomascountz"))
-      User.find_by!(github_handle: "tripple-a").assign_canonical_user!(canonical_user: User.find_by!(github_handle: "Tripple-A"))
-      User.find_by!(github_handle: "winslett").assign_canonical_user!(canonical_user: User.find_by!(github_handle: "Winslett"))
+      User.find_by(github_handle: "bodacious")&.assign_canonical_user!(canonical_user: User.find_by(github_handle: "Bodacious"))
+      User.find_by(github_handle: "joschkaschulz")&.assign_canonical_user!(canonical_user: User.find_by(github_handle: "JoschkaSchulz"))
+      User.find_by(github_handle: "kyfast")&.assign_canonical_user!(canonical_user: User.find_by(github_handle: "KyFaSt"))
+      User.find_by(github_handle: "nabeelahy")&.assign_canonical_user!(canonical_user: User.find_by(github_handle: "NabeelahY"))
+      User.find_by(github_handle: "ryanbrushett")&.assign_canonical_user!(canonical_user: User.find_by(github_handle: "RyanBrushett"))
+      User.find_by(github_handle: "thomascountz")&.assign_canonical_user!(canonical_user: User.find_by(github_handle: "Thomascountz"))
+      User.find_by(github_handle: "tripple-a")&.assign_canonical_user!(canonical_user: User.find_by(github_handle: "Tripple-A"))
+      User.find_by(github_handle: "winslett")&.assign_canonical_user!(canonical_user: User.find_by(github_handle: "Winslett"))
     end
     # Now add the case-insensitive unique index
     add_index :users, "lower(github_handle)", name: "index_users_on_lower_github_handle",
