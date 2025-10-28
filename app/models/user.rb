b1d8c7ca9e6d22213d03178acc512bbaa71e1800
Adrian Marin <adrian@adrianthedev.com>
2025-10-28 22:08:06 +0200

    Implement `User#to_param` and event tweaks (#1112)
    Co-authored-by: Marco Roth <marco.roth@intergga.ch>

--- app/models/user.rb
+++ app/models/user.rb
@@ -352,6 +352,10 @@ class User < ApplicationRecord
     (handles.count == 1) ? handles.first : nil
   end
 
+  def to_param
+    github_handle || slug
+  end
+
   private
 
   def seed_development_watched_talks
